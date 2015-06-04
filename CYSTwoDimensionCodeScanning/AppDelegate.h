3e25baed499ecf7d5d5bcb1f3e4b6c4bc8da7d8b
陈远山 <chenysh521@126.com>
2015-06-04 11:03:23 +0800

    twodimensionCode

---
+++ CYSTwoDimensionCodeScanning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYSTwoDimensionCodeScanning
+//
+//  Created by YanceyChan on 15/6/4.
+//  Copyright (c) 2015年 exmaple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYSTwoDimensionCodeScanning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYSTwoDimensionCodeScanning
+//
+//  Created by YanceyChan on 15/6/4.
+//  Copyright (c) 2015年 exmaple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
